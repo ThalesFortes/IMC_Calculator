8f0ddc270e8fcf05080046cc4a1676a077282829
Thales de Abreu Fortes da Silva <thales_pro@outlook.com>
2024-04-24 21:50:20 -0300

    Pegando os inputs e classes necessarias

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,4 @@
-document.querySelector()
+const modalWrapper = document.querySelector(".modal_wrapper");
+const erroAlert = document.querySelector(".alert_error");
+let weight = document.getElementById("weight");
+let height = document.getElementById("height")
